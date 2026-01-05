d24c8516f81653c966228959a0da4bf20b14e99d
Maxime Gouraud <gouraudmaxime@orange.fr>
2026-01-05 21:24:42 +0100

    Add ft_strlen, not working yet

--- src/ft_strlen.s
+++ src/ft_strlen.s
@@ -1,3 +1,17 @@
 ; cmp rdi, 0	: Compare value in rdi with 0
 ; inc rcx		: Increment value in rcx by 1
 ; je label		: Jump to label if comparison was equal
+
+global ft_strlen			; Declare as global for external use
+
+ft_strlen:
+	mov	 rcx, 0				; Initialize length counter to 0 in rcx, usually used for counters
+.loop:
+	cmp	 [rdi + rcx], 0		; Compare byte at address (rdi + rcx) with 0 (null char).
+							; rdi is a reg. that typically holds the ptr to the function first argument (here, the string)
+	je	 .end				; If the byte is 0, jump to .end
+	inc	 rcx				; Increment length counter
+	jmp	 .loop				; Repeat the loop
+.end:
+	mov	 rax, rcx			; Move the length from rcx to rax (return value)
+	ret						; Return from function
