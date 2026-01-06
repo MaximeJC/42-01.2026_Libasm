acae6c9d7e4c1f0b7f804a226e58c0e5230ab567
Maxime Gouraud <gouraudmaxime@orange.fr>
2026-01-06 15:26:28 +0100

    Added includes and asm functions to header file

--- libasm.h
+++ libasm.h
@@ -1,4 +1,17 @@
 #ifndef LIBASM_H
 #define LIBASM_H
 
+#include <stdio.h>		// For printf
+#include <unistd.h>		// For write, read
+#include <stdlib.h>		// For malloc, free
+#include <string.h>		// For strlen, strcmp, strcpy, strdup
+#include <fcntl.h>		// For file open flags
+
+size_t	ft_strlen(const char *s);							// size_t strlen(const char *s);
+char	*ft_strcpy(char *dest, const char *src);			// char *strcpy(char *restrict dst, const char *restrict src);
+int		ft_strcmp(const char *s1, const char *s2);			// int strcmp(const char *s1, const char *s2);
+ssize_t	ft_write(int fd, const void *buf, size_t count);	// ssize_t write(int fd, const void buf[], size_t count);
+ssize_t	ft_read(int fd, void *buf, size_t count);			// ssize_t read(int fd, void buf[], size_t count);
+char	*ft_strdup(const char *s);							// char *strdup(const char *s);
+
 #endif // LIBASM_H
