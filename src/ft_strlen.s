b946574c4ea501fb69fbc5f42e36aeaba7f660c9
Maxime Gouraud <gouraudmaxime@orange.fr>
2026-01-06 17:41:36 +0100

    Reindent file

--- src/ft_strlen.s
+++ src/ft_strlen.s
@@ -4,15 +4,15 @@
 global ft_strlen			; Declare as global for external use
 
 ft_strlen:
-	mov	 rcx, 0				; Initialize length counter to 0 in rcx, usually used for counters
+	mov	rcx, 0				; Initialize length counter to 0 in rcx, usually used for counters
 .loop:
-	cmp	 BYTE [rdi + rcx], 0	; Compare byte at address (rdi + *rcx) with 0 (null char).
+	cmp	BYTE [rdi + rcx], 0	; Compare byte at address (rdi + *rcx) with 0 (null char).
 							; rdi is a reg. that typically holds the ptr to the function first argument (here, the string)
-	je	 .end				; If the byte is 0, jump to .end
-	inc	 rcx				; Increment length counter
-	jmp	 .loop				; Repeat the loop
+	je	.end				; If the byte is 0, jump to .end
+	inc	rcx					; Increment length counter
+	jmp	.loop				; Repeat the loop
 .end:
-	mov	 rax, rcx			; Move the length from rcx to rax (return value)
+	mov	rax, rcx			; Move the length from rcx to rax (return value)
 	ret						; Return from function
 
 ; To avoid warnings from linker about stack section
