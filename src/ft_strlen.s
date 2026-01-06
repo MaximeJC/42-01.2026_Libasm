607ab86d5b19ee4fbbac276f88992a96c8fa20a5
Maxime Gouraud <gouraudmaxime@orange.fr>
2026-01-06 15:18:31 +0100

    ft_strlen now working

--- src/ft_strlen.s
+++ src/ft_strlen.s
@@ -3,7 +3,7 @@ global ft_strlen			; Declare as global for external use
 ft_strlen:
 	mov	 rcx, 0				; Initialize length counter to 0 in rcx, usually used for counters
 .loop:
-	cmp	 [rdi + rcx], 0		; Compare byte at address (rdi + rcx) with 0 (null char).
+	cmp	 BYTE [rdi + rcx], 0	; Compare byte at address (rdi + *rcx) with 0 (null char).
 							; rdi is a reg. that typically holds the ptr to the function first argument (here, the string)
 	je	 .end				; If the byte is 0, jump to .end
 	inc	 rcx				; Increment length counter
@@ -12,4 +12,4 @@ ft_strlen:
 	mov	 rax, rcx			; Move the length from rcx to rax (return value)
 	ret						; Return from function
 
-; TODO Still not working, error line 6: "impossible combination of address sizes"
+
