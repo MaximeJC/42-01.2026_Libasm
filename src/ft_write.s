2df05c512f655a88783b6379766c10effc9d6967
Maxime Gouraud <mgouraud@student.42lehavre.fr>
2026-01-07 16:14:50 +0100

    Fix ft_write, finally working

--- src/ft_write.s
+++ src/ft_write.s
@@ -1,19 +1,21 @@
 ; ssize_t	ft_write(int fd, const void *buf, size_t count);
 ;	rax					 rdi			 rsi		  rdx
 
-global	ft_write			; Declare as global for external use
-extern	__errno_location	; To be able to use C errno value
+global	ft_write						; Declare as global for external use
+extern	__errno_location				; To be able to use C errno value
 
 ft_write:
-	mov		rax, 4				; Syscall number for sys_write
-	syscall
-	jc		.error
+	mov		rax, 1						; Syscall number for sys_write
+	syscall								; Call system write
+	cmp		rax, 0
+	jl		.error						; Jump if in previous cmp op1 was strictly lower than op2
 	ret
 .error:
-	mov		r8, rax				; Saving rax as it is our error value
-	call	__errno_location	; Put errno address into rax
-	mov		[rax], r8			; Put back our error code into rax pointed value
-	mov		rax, -1				; Setting return rax value to -1
+	neg		rax							; Errno code is given in negative form, so you have to invert it
+	mov		r8, rax						; Saving rax as it is our error value
+	call	__errno_location wrt ..plt	; Put errno address into rax | "wrt ..plt" needed to correct "relocation R_X86_64_PC32" error
+	mov		[rax], r8					; Put back our error code into rax pointed value
+	mov		rax, -1						; Setting return rax value to -1
 	ret
 
 ; To avoid warnings from linker about stack section
