669b39ed681d582379bee2f15a37b31988d70973
Maxime Gouraud <mgouraud@student.42lehavre.fr>
2026-01-08 15:31:31 +0100

    Fix ft_strcpy: Now correctly set null-terminating byte

--- src/ft_strcpy.s
+++ src/ft_strcpy.s
@@ -6,14 +6,15 @@ global ft_strcpy			; Declare as global for external use
 ft_strcpy:
 	mov	rcx, 0					; Initialize index counter to 0 in rcx
 .copy_loop:
+	cmp	BYTE [rsi + rcx], 0		; Check if end of string
+	je	.end
 	mov	al, BYTE [rsi + rcx]	; We can't move from one address to another, we need a temp var to store the byte, here al
 	mov	BYTE [rdi + rcx], al	; al is a reg meant to store a single byte
 	inc	rcx						; Increment counter
-	cmp	BYTE [rsi + rcx], 0		; Check if end of string
-	jne	.copy_loop				; Jump if not equal, can replace the three following lines:
-								; 	 	je	.loop_end	- End loop if null char of src
-								; 		jmp	.copy_loop	- Else, continue loop
-								; 	.loop_end:
+	jmp	.copy_loop				; Jump to loop again
+.end:
+	mov	al, BYTE [rsi + rcx]	; Copy NULL byte
+	mov	BYTE [rdi + rcx], al
 	mov	rax, rdi				; Setting dest ptr as the return value
 	ret
 
